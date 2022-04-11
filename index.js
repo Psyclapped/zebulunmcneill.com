d040acfa601b9ab5c00b69bb5c276d594714f640
Zebulun McNeill <zeb.mcn@gmail.com>
2022-04-11 11:14:20 -0500

    commenting out hidden link functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const jokeLink = document.getElementById("of");
+/* const jokeLink = document.getElementById("of");
 
 jokeLink.addEventListener("click", (e) => {
   window.alert("You found something interesting. Here's a friendly reminder that if you use this against innocent people, I will personally break down your door and take a fat shit down your throat.");
   window.open("https://nimble-basbousa-78a37b.netlify.app/", "_blank");
-})
+}) */
