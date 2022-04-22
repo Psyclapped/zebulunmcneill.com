a1a19fff4cdcbccea869480073f3150234a4304b
Zebulun McNeill <zeb.mcn@gmail.com>
2022-04-21 22:31:58 -0500

    adding togglable (and memorized) darkmode

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const darkMode = document.getElementById("darkMode");
 const body = document.getElementsByTagName("body")[0];
 
+if (localStorage.getItem("darkMode") === "true") {
+  body.classList.add("darkMode");
+}
+
 darkMode.addEventListener("click", (e) => {
   body.classList.toggle("darkMode");
+  if (localStorage.getItem("darkMode") === "true") {
+    localStorage.setItem("darkMode", "false");
+  } else {
+    localStorage.setItem("darkMode", "true");
+  }
 })
