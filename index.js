a03b0180922c72e91faa1b0282c7d6ec0d1b3b96
Zebulun McNeill <zeb.mcn@gmail.com>
2022-04-21 22:28:22 -0500

    adding glow effects

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-/* const jokeLink = document.getElementById("of");
+const darkMode = document.getElementById("darkMode");
+const body = document.getElementsByTagName("body")[0];
 
-jokeLink.addEventListener("click", (e) => {
-  window.alert("You found something interesting. Here's a friendly reminder that if you use this against innocent people, I will personally break down your door and take a fat shit down your throat.");
-  window.open("https://nimble-basbousa-78a37b.netlify.app/", "_blank");
-}) */
+darkMode.addEventListener("click", (e) => {
+  body.classList.toggle("darkMode");
+})
